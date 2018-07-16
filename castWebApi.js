306fd60cbaa5ec8d7b6a391d5ee8e90d4dc478e1
Ola Eriksson <nanab666@gmail.com>
2018-07-16 10:18:05 +0200

    Added header to allow cross reference
    Added header to allow cross reference

--- castWebApi.js
+++ castWebApi.js
@@ -116,6 +116,9 @@ function createWebServer() {
 			log('debug-server', 'requestData', requestData);
 
 			res.setHeader('Content-Type', 'application/json; charset=utf-8');
+			res.setHeader("Access-Control-Allow-Origin", "*");
+  			res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+			res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
 
 			if (path[1]=="device") {
 				if (path[2]) {
